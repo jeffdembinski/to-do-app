f59bf99ffefb86002706f1a15d6457e9ce9f724c
Jeff Dembinski <jeff@magicquotes.io>
2017-10-01 17:51:53 -0400

    To Do Item generates a delete X and unique ID for each to do item

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ function onReady() {
   function createNewToDo() {
     var newToDoText = document.getElementById('newToDoText');
     toDos.push({
+      id: (new Date()).getTime(),
       title: newToDoText.value,
       complete: false
     });
@@ -18,10 +19,18 @@ function onReady() {
     toDos.forEach(function(toDo){
       var newLi = document.createElement('li');
       var checkbox = document.createElement('input');
+      var spanDelete = document.createElement('span');
+      spanDelete.setAttribute("id", toDo.id);
+      spanDelete.innerHTML = "&nbsp;&#10007;&nbsp;";
       checkbox.type = "checkbox";
       newLi.innerHTML = toDo.title;
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(spanDelete);
+      spanDelete.onclick = function deleteItem(e) {
+        var id = e.target.id;
+        console.log("Delete an item: " + id);
+      }
     });
   }
 
